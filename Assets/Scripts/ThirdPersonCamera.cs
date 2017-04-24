cf9af7d3fdff6194959ec4d28800926dc5e5006e
Simon Chauvin <contact@simonchauvin.com>
2017-04-24 20:13:59 +0200

    Nicer camera look at.

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -8,13 +8,20 @@ public class ThirdPersonCamera : MonoBehaviour
     public float followSpeed;
     public float followHeightSpeed;
     public float rotationSpeed;
-    public float smoothTime;
-    public float minDistanceHeight;
-    public float maxDistanceHeight;
-    public float minDistance;
-    public float maxDistance;
+    public float startMinDistance;
+    public float startMaxDistance;
+    public float startMinDistanceHeight;
+    public float startMaxDistanceHeight;
+    public Vector2 rotationRange;
 
+    private Quaternion originalRotation;
+    private Vector3 followAngles;
+    private Vector3 followVelocity;
     private bool hasStarted;
+    private float minDistance;
+    private float maxDistance;
+    private float minDistanceHeight;
+    private float maxDistanceHeight;
 
 
     void Start ()
@@ -24,7 +31,14 @@ public class ThirdPersonCamera : MonoBehaviour
 
     public void init ()
     {
-        transform.position = Player.instance.getFrontPosition() + new Vector3(0f, minDistanceHeight, 0f);
+        minDistance = startMinDistance;
+        maxDistance = startMaxDistance;
+        minDistanceHeight = startMinDistanceHeight;
+        maxDistanceHeight = startMaxDistanceHeight;
+
+        transform.position = Player.instance.getFrontPosition() + new Vector3(minDistance, minDistanceHeight, minDistance);
+        transform.rotation = Quaternion.LookRotation(Player.instance.getFrontPosition() - transform.position);
+        originalRotation = transform.rotation;
         hasStarted = true;
     }
 	
@@ -35,15 +49,16 @@ public class ThirdPersonCamera : MonoBehaviour
             // Follow player
             Vector3 playerPos = Player.instance.getFrontPosition();
             Vector3 direction = playerPos - transform.position;
-            Vector3 planarDirection = new Vector3(direction.x, 0f, direction.z).normalized;
-            Vector3 playerPlanarDirection = new Vector3(direction.x, playerPos.y, direction.z);
-            if (playerPlanarDirection.magnitude >= maxDistance)
+            Debug.DrawRay(transform.position, direction, Color.red);
+            Vector3 planarDirection = new Vector3(direction.x, 0f, direction.z);
+            Debug.DrawRay(transform.position, planarDirection, Color.blue);
+            if (planarDirection.magnitude >= maxDistance)
             {
-                transform.position += planarDirection * followSpeed * Time.deltaTime;
+                transform.position += planarDirection.normalized * followSpeed * Time.deltaTime;
             }
-            else if (playerPlanarDirection.magnitude < minDistance)
+            else if (planarDirection.magnitude < minDistance)
             {
-                transform.position -= planarDirection * followSpeed * Time.deltaTime;
+                transform.position -= planarDirection.normalized * followSpeed * Time.deltaTime;
             }
 
             if (transform.position.y - playerPos.y < minDistanceHeight)
@@ -56,10 +71,38 @@ public class ThirdPersonCamera : MonoBehaviour
             }
 
             // Looking at the player
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+            lookAt(playerPos);
 
             // Unzooming
             //target.position = new Vector3(transform.position.x, transform.position.y + unzoomSpeed * Time.deltaTime, transform.position.z);
+            //minDistanec *= unzoomspeed * time.deltatime;
         }
+    }
+
+    protected void lookAt (Vector3 playerPos)
+    {
+        // we make initial calculations from the original local rotation
+        transform.localRotation = originalRotation;
+
+        // tackle rotation around Y first
+        Vector3 localTarget = transform.InverseTransformPoint(playerPos);
+        float yAngle = Mathf.Atan2(localTarget.x, localTarget.z) * Mathf.Rad2Deg;
+
+        yAngle = Mathf.Clamp(yAngle, -rotationRange.y * 0.5f, rotationRange.y * 0.5f);
+        transform.localRotation = originalRotation * Quaternion.Euler(0, yAngle, 0);
+
+        // then recalculate new local target position for rotation around X
+        localTarget = transform.InverseTransformPoint(playerPos);
+        float xAngle = Mathf.Atan2(localTarget.y, localTarget.z) * Mathf.Rad2Deg;
+        xAngle = Mathf.Clamp(xAngle, -rotationRange.x * 0.5f, rotationRange.x * 0.5f);
+        var targetAngles = new Vector3(followAngles.x + Mathf.DeltaAngle(followAngles.x, xAngle),
+                                       followAngles.y + Mathf.DeltaAngle(followAngles.y, yAngle));
+
+        // smoothly interpolate the current angles to the target angles
+        followAngles = Vector3.SmoothDamp(followAngles, targetAngles, ref followVelocity, rotationSpeed);
+
+
+        // and update the gameobject itself
+        transform.localRotation = originalRotation * Quaternion.Euler(-followAngles.x, followAngles.y, 0);
     }
 }
